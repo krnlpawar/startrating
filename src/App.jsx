796beba235b1bcf10c79f45b9252bb0f92abcfad
Krunal Pawar <krunal.pawar@neosoftmail.com>
2024-12-31 17:08:40 +0530

    worked on star rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [rating, setRating] = useState(0)
+
+  const handleRating = (e) => {
+    setRating(e.target.dataset.rating)
+  }
+
   return (
     <>
-      <p>Hello world</p>
+      <div className="star-rating">
+        {[...Array(5)].map((star, index) => {
+          return <span className={index < rating ? 'star rated' : 'star'} data-rating={index + 1} onClick={handleRating}>&#9733;</span>
+        })}
+      </div>
     </>
   )
 }
